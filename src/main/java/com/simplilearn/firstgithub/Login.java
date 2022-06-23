5aa6d3f9e20edd4e7747d51105539c4d7390d4b3
TEJASWINI <TEJASWINI@192.168.1.4>
2022-06-23 23:53:31 +0530

    MY FIRST COMMIT

---
+++ src/main/java/com/simplilearn/firstgithub/Login.java
@@ -0,0 +1,17 @@
+package com.simplilearn.firstgithub;
+
+class myfirstgithub
+{
+	public static void main(String[]args)
+	{
+		
+		System.out.print("This is my firstgithub project");
+	}
+	
+	    void test()
+	    {
+		System.out.println("This is test A");
+	    }
+
+
+}
